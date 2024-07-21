97526217eb8bc68d496afff8f6091b89b20aab46
rhtAnujMody <anuj.mody@revealhealthtech.com>
2024-07-22 00:16:21 +0530

    first commit

---
+++ src/components/Auth.tsx
@@ -0,0 +1,12 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+
+const Auth = () => {
+  return (
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Text>Auth Component</Text>
+    </View>
+  );
+};
+
+export default Auth;
---
+++ src/components/EmbeddedComponents/index.ts
@@ -0,0 +1,1 @@
+export {default as ECImageView} from './ImageView';
