8661d8617dd5ecc9f27a138a3e6e4b03545a5592
rhtAnujMody <anuj.mody@revealhealthtech.com>
2024-07-22 12:49:58 +0530

    added survey parent

---
+++ src/components/reusable-components/Survey/SurveyParent.tsx
@@ -0,0 +1,10 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+
+export default function SurveyParent() {
+  return (
+    <View>
+      <Text>SurveyParent</Text>
+    </View>
+  );
+}
--- src/components/reusable-components/Survey/index.ts
+++ src/components/reusable-components/Survey/index.ts
@@ -1,3 +1,4 @@
 export {default as Question} from './Question';
 export {default as Checkbox} from './CheckboxQuestionComponent';
 export {default as Radio} from './RadioQuestionComponent';
+export {default as SurveyParent} from './SurveyParent';
