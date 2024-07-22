6ace2d899659145e8dd87c0d5936b097e4cfc28f
rhtAnujMody <anuj.mody@revealhealthtech.com>
2024-07-22 14:39:05 +0530

    added theme

---
+++ src/theme/Theme.ts
@@ -0,0 +1,24 @@
+export default {
+  colors: {
+    primaryColor: '#000FEA0',
+    secondaryColor: '#000110',
+  },
+  flex: {
+    flex: 1,
+  },
+  center: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  fontSize: {
+    header: {
+      fontSize: '20px',
+    },
+    desc: {
+      fontSize: '16px',
+    },
+  },
+  fontFamily: {
+    fontFamily: 'ROBOTO',
+  },
+};
